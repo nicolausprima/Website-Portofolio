1bbde5cf4724af6896ffda12ddaf921583c9e253
Nicolaus Prima Dharma <dedyputera223@gmail.com>
2025-08-17 12:29:12 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var typed = new Typed('.auto-type', {
+    strings: ["Hi, my name is <ins>Nicolaus Prima.</ins>"],
+    typeSpeed:50,
+    backSpeed:50,
+    loop: true,
+    startDelay: 2000,
+    backDelay: 3000,
+    showCursor: false,
+    })
